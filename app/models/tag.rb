852c317ce191d3df54b62f384e99686c52c9340a
icdozen <icdozen@gmail.com>
2016-04-02 17:41:52 -0400

    search & color controller def

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,14 @@
 class Tag < ActiveRecord::Base
+  PROPERTIES = [:timestamp, :id, :name]
+  PROPERTIES.each { |prop|
+    attr_accessor prop
+  }
+
+  def initialize(hash = {})
+    hash.each { |key, value|
+      if PROPERTIES.member? key.to_sym
+        self.send((key.to_s + "=").to_s, value)
+      end
+    }
+  end
 end
